ae0f063ffc06a9566616e20c6ad23e1c7be763fc
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2023-12-11 07:54:05 +0100

    Add custom styles to tailwind
    Customized breakpoints to be based on rem units for better responsive
    experience. Added custom colors and font family.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,29 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    screens: {
+      xs: '36rem',
+      sm: '48rem',
+      md: '64rem',
+      lg: '85.375rem',
+      xl: '120rem',
+      '2xl': '160rem',
+    },
+    extend: {
+      colors: {
+        dark: {
+          900: '#131313',
+          600: '#2C2C2C',
+          500: '#7D7D7D',
+        },
+        primary: {
+          600: '#4E2D79',
+        },
+      },
+      fontFamily: {
+        main: ['Inter', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
