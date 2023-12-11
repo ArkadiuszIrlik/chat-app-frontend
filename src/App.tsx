c97946dc1631f599b3ab08f0b8c74153e7055890
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2023-12-11 11:07:04 +0100

    Refactor App component
    Added responsive styling for higher resolutions, a dark background
    color and a column display of content.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import ChatScreen from '@containers/ChatScreen/ChatScreen';
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div className="bg-dark-900">
+      <div
+        className="mx-auto flex min-h-screen flex-col
+       font-main lg:mx-[8%] xl:mx-[14%] 2xl:mx-auto 2xl:max-w-[1800px]"
+      >
+        <ChatScreen />
+      </div>
+    </div>
+  );
 }
 
 export default App;
