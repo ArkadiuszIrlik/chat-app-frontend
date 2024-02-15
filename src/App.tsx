059742eac938f5a4e89f07434093bccad9eaf691
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2024-02-15 16:59:55 +0100

    Update App component styles

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,10 @@ import useSocketStatus from '@hooks/useSocketStatus';
 function App() {
   const isSocketConnected = useSocketStatus();
   return (
-    <div className="bg-dark-900">
+    <div className="bg-gray-800">
       <div
-        className="mx-auto flex min-h-screen flex-col
-       font-ui lg:mx-[8%] xl:mx-[14%] 2xl:mx-auto 2xl:max-w-[1800px]"
+        className="mx-auto flex min-h-screen flex-col overflow-hidden font-ui
+       text-white lg:mx-[8%] xl:mx-[14%] 2xl:mx-auto 2xl:max-w-[1800px]"
       >
         <ChatScreen />
         <p className="text-white">
