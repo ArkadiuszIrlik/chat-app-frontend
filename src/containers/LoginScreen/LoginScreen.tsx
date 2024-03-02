e035a4bd245f32062478502165484143064cda32
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2024-03-02 18:27:48 +0100

    Add LoginScreen

---
+++ src/containers/LoginScreen/LoginScreen.tsx
@@ -0,0 +1,57 @@
+import WaveImage from '@assets/wave.svg';
+import LoginForm from '@components/LoginForm/LoginForm';
+import { useMediaQuery } from '@uidotdev/usehooks';
+import styleConsts from '@constants/styleConsts';
+
+function LoginScreen() {
+  const isMediumScreen = useMediaQuery(
+    `only screen and (min-width: ${styleConsts.screens.md}`,
+  );
+  return (
+    <div className="relative flex h-full min-h-screen">
+      {isMediumScreen && (
+        <div className="flex-1">
+          <div className="sticky top-0 flex flex-col overflow-hidden">
+            <div className="relative aspect-[628/498]">
+              <img
+                src={WaveImage}
+                alt=""
+                srcSet=""
+                draggable="false"
+                className="visual-search-none absolute bottom-0 left-1/2
+                 right-0 top-0 w-[250%] max-w-none -translate-x-1/2
+                  -translate-y-[20%] -rotate-6 select-none object-center"
+              />
+              <div className="absolute left-0 right-0 top-1/4">
+                <div
+                  className="beluga-mask mx-auto aspect-square
+                 w-1/2 bg-gradient-to-tr from-clairvoyant-900
+                  to-cerise-600 text-white"
+                />
+              </div>
+            </div>
+            <div className="left-0 right-1/2 top-[68%] shrink grow-[1] ">
+              <p
+                className="mb-2 text-center font-beluga text-5xl text-white
+               xl:text-7xl"
+              >
+                Beluga
+              </p>
+            </div>
+          </div>
+        </div>
+      )}
+      <div className="flex-1 bg-gray-700">
+        <div
+          className="flex flex-col
+       items-center justify-center py-3 sm:px-10 sm:py-20"
+        >
+          <div className="w-64">
+            <LoginForm />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default LoginScreen;
