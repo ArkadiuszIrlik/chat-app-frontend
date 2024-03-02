209fe3b48dd9e3a5e13a9cc150c9c4fdd0d247e5
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2024-03-02 19:57:19 +0100

    Refactor App component
    The App component will now serve as the root layout for react router.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
-import ChatScreen from '@containers/ChatScreen/ChatScreen';
-import useSocketStatus from '@hooks/useSocketStatus';
+// import useSocketStatus from '@hooks/useSocketStatus';
+import { Outlet } from 'react-router-dom';
 
 function App() {
-  const isSocketConnected = useSocketStatus();
+  // const isSocketConnected = useSocketStatus();
   return (
     <div className="bg-gray-800">
       <div
-        className="mx-auto flex min-h-screen flex-col overflow-hidden font-ui
+        className="mx-auto flex min-h-screen flex-col overflow-clip font-ui
        text-white lg:mx-[8%] xl:mx-[14%] 2xl:mx-auto 2xl:max-w-[1800px]"
       >
-        <ChatScreen />
-        <p className="text-white">
-          {isSocketConnected ? 'Connected' : 'Not Connected'}
-        </p>
+        <Outlet />
       </div>
     </div>
   );
