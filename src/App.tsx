e21ab261c1f036d341c559cfbcb19806eb9f470f
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2024-03-14 23:17:33 +0100

    Add AuthProvider component
    This component uses React context api to provide the useAuth return
    object to the entire app.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 // import useSocketStatus from '@hooks/useSocketStatus';
 import { Outlet } from 'react-router-dom';
+import AuthProvider from '@components/AuthProvider/AuthProvider';
 
 function App() {
   // const isSocketConnected = useSocketStatus();
   return (
-    <div className="bg-gray-800">
-      <div
-        className="mx-auto flex min-h-screen flex-col overflow-clip font-ui
-       text-white lg:mx-[8%] xl:mx-[14%] 2xl:mx-auto 2xl:max-w-[1800px]"
-      >
-        <Outlet />
+    <AuthProvider>
+      <div className="bg-gray-800">
+        <div
+          className="mx-auto flex min-h-screen flex-col overflow-clip font-ui
+         text-white lg:mx-[8%] xl:mx-[14%] 2xl:mx-auto 2xl:max-w-[1800px]"
+        >
+          <Outlet />
+        </div>
       </div>
-    </div>
+    </AuthProvider>
   );
 }
 
