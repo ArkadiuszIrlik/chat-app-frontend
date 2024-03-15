342dbb8ecbd4f370ff9f60369f611f1c615fef8c
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2024-03-15 05:05:19 +0100

    Add performance optimizations to useAuth
    Memoized the return object to prevent unnecessary rerenders of
    components consuming "auth" context.

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,5 +1,5 @@
 import { getURL } from '@helpers/fetch';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 
 interface UserAuth {
   _id: string;
@@ -10,10 +10,24 @@ function useAuth() {
   const [user, setUser] = useState<UserAuth | null>(null);
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
+  const login = useCallback(async () => {
+    try {
+      const res = await fetch(getURL('/auth/user'));
+      const nextUser = (await res.json()) as UserAuth;
+      setUser(nextUser);
+    } catch {
+      setUser(null);
+    }
+  }, [setUser]);
+
+  const logout = useCallback(() => {
+    setUser(null);
+  }, [setUser]);
+
   useEffect(() => {
     // eslint-disable-next-line no-void
     void login();
-  }, []);
+  }, [login]);
 
   useEffect(() => {
     if (user !== null) {
@@ -23,26 +37,16 @@ function useAuth() {
     }
   }, [user]);
 
-  async function login() {
-    try {
-      const res = await fetch(getURL('/auth/user'));
-      const nextUser = (await res.json()) as UserAuth;
-      setUser(nextUser);
-    } catch {
-      setUser(null);
-    }
-  }
-
-  function logout() {
-    setUser(null);
-  }
-
-  return {
-    user,
-    isAuthenticated,
-    logout,
-    login,
-  };
+  const authObj = useMemo(
+    () => ({
+      user,
+      isAuthenticated,
+      logout,
+      login,
+    }),
+    [user, isAuthenticated, logout, login],
+  );
+  return authObj;
 }
 
 export default useAuth;
