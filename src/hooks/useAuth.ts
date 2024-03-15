6b1900fcce134358e93e4040fa8a12dcdecc5284
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2024-03-15 04:22:44 +0100

    Refactor useAuth hook

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { getURL } from '@helpers/fetch';
+import { useEffect, useState } from 'react';
 
 interface UserAuth {
   _id: string;
@@ -7,18 +8,35 @@ interface UserAuth {
 
 function useAuth() {
   const [user, setUser] = useState<UserAuth | null>(null);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-  function login(id: string, name: string) {
-    setUser({ _id: id, name });
+  useEffect(() => {
+    // eslint-disable-next-line no-void
+    void login();
+  }, []);
+
+  useEffect(() => {
+    if (user !== null) {
+      setIsAuthenticated(true);
+    } else {
+      setIsAuthenticated(false);
+    }
+  }, [user]);
+
+  async function login() {
+    try {
+      const res = await fetch(getURL('/auth/user'));
+      const nextUser = (await res.json()) as UserAuth;
+      setUser(nextUser);
+    } catch {
+      setUser(null);
+    }
   }
 
   function logout() {
     setUser(null);
   }
 
-  function isAuthenticated() {
-    return user !== null;
-  }
   return {
     user,
     isAuthenticated,
