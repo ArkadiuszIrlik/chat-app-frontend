eb88364e825a1381960b7bfae6546ad2f8f245ad
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2024-03-20 14:07:46 +0100

    Refactor socket connection rules
    Sockets will now send auth cookies during the handshake.
    Also connection won't be auto-established on homescreen, instead
    allowing the user to first login and provide valid authentication.

--- src/helpers/socket.ts
+++ src/helpers/socket.ts
@@ -1,4 +1,7 @@
 import { io } from 'socket.io-client';
 
-const socket = io(import.meta.env.VITE_SOCKET_IO_URL);
+const socket = io(import.meta.env.VITE_SOCKET_IO_URL, {
+  autoConnect: false,
+  withCredentials: true,
+});
 export default socket;
