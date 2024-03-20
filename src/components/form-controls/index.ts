53e205b5eebb44b0ee813410846151697119fec6
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2024-03-20 14:43:00 +0100

    Update index.ts export files

---
+++ src/components/form-controls/index.ts
@@ -0,0 +1,4 @@
+export * from './TextInput';
+export { default as TextInput } from './TextInput';
+export * from './SubmitButtonPrimary';
+export { default as SubmitButtonPrimary } from './SubmitButtonPrimary';
