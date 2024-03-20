e9ae47fe2ff55d00d50aec3680663b14901fea53
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2024-03-20 13:52:23 +0100

    Add socket authentication failure handler

--- src/hooks/useSocketStatus.ts
+++ src/hooks/useSocketStatus.ts
@@ -1,29 +1,34 @@
 import { useEffect, useState } from 'react';
 import socket from '@helpers/socket';
+import { useAuth } from '@hooks/index';
 
 function useSocketStatus() {
   const [isConnected, setIsConnected] = useState(socket.connected);
+  const { logout } = useAuth()!;
 
   useEffect(() => {
-    socket.connect();
+      socket.connect();
 
-    function onConnect() {
-      setIsConnected(true);
-    }
+      function onConnect() {
+        setIsConnected(true);
+      }
 
-    function onDisconnect() {
-      setIsConnected(false);
-    }
+      function onDisconnect() {
+        setIsConnected(false);
+      }
 
-    socket.on('connect', onConnect);
-    socket.on('disconnect', onDisconnect);
+      socket.on('connect', onConnect);
+      socket.on('disconnect', onDisconnect);
+      socket.on('authentication error', () => {
+        void logout();
+      });
 
-    return () => {
-      socket.off('connect', onConnect);
-      socket.off('disconnect', onDisconnect);
-      socket.disconnect();
-    };
-  }, []);
+      return () => {
+        socket.off('connect', onConnect);
+        socket.off('disconnect', onDisconnect);
+        socket.disconnect();
+      };
+  }, [logout]);
 
   return isConnected;
 }
