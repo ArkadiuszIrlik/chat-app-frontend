dd73da2c0bb2c0d44339bade32478fc27ffb24bb
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2024-04-04 22:46:00 +0200

    Add event handlers to useSocket
    Refactored useSocket hook to process incoming chat message events and
    return them in a messageEvents state array.

--- src/hooks/useSocket.ts
+++ src/hooks/useSocket.ts
@@ -2,9 +2,14 @@ import { useEffect, useState } from 'react';
 import socket from '@helpers/socket';
 import { useAuth } from '@hooks/index';
 
+interface NetworkMessage extends Omit<Message, 'postedAt'> {
+  postedAt: string;
+}
+
 function useSocket() {
-  const [isConnected, setIsConnected] = useState(socket.connected);
   const { logout } = useAuth()!;
+  const [isConnected, setIsConnected] = useState(socket.connected);
+  const [messageEvents, setMessageEvents] = useState<Message[]>([]);
 
   useEffect(() => {
     socket.connect();
@@ -19,17 +24,35 @@ function useSocket() {
 
     socket.on('connect', onConnect);
     socket.on('disconnect', onDisconnect);
-    socket.on('authentication error', () => {
-      void logout();
-    });
 
     return () => {
       socket.off('connect', onConnect);
       socket.off('disconnect', onDisconnect);
       socket.disconnect();
     };
-  }, [logout]);
+  }, []);
 
-  return isConnected;
+  useEffect(() => {
+    function onMessageEvent(nextEvent: NetworkMessage) {
+      const nextMessageEvent: Message = {
+        ...nextEvent,
+        postedAt: new Date(nextEvent.postedAt),
+      };
+      setMessageEvents([...messageEvents, nextMessageEvent]);
+    }
+
+    function onAuthError() {
+      void logout();
+    }
+
+    socket.on('chat message', onMessageEvent);
+    socket.on('authentication error', onAuthError);
+    return () => {
+      socket.off('chat message', onMessageEvent);
+      socket.off('authentication error', onAuthError);
+    };
+  }, [logout, messageEvents]);
+
+  return { isConnected, messageEvents };
 }
 export default useSocket;
