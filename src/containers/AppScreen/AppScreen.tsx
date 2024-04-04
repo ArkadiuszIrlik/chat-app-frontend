34cd9b04f8520c451b33f8d8c5fac0170cc6b702
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2024-04-04 22:21:02 +0200

    Add MessageEventsProvider to AppScreen

--- src/containers/AppScreen/AppScreen.tsx
+++ src/containers/AppScreen/AppScreen.tsx
@@ -1,5 +1,5 @@
 import { NotificationDisplay } from '@components/NotificationDisplay';
-import { useAuth, useSocket } from '@hooks/index';
+import { useAuth, useSocket, MessageEventsProvider } from '@hooks/index';
 import ServerSelectBar from '@containers/AppScreen/ServerSelectBar';
 import { Outlet } from 'react-router-dom';
 
@@ -8,20 +8,22 @@ function AppScreen() {
   const { messageEvents } = useSocket();
 
   return (
-    <div className="flex min-h-screen">
-      <NotificationDisplay messageEvents={messageEvents} />
-      <button
-        type="button"
-        className="bg-red-500 p-4"
-        onClick={() => {
-          void logout();
-        }}
-      >
-        Log Out
-      </button>
-      <ServerSelectBar serverList={user?.serversIn ?? []} />
-      <Outlet />
-    </div>
+    <MessageEventsProvider>
+      <div className="flex min-h-screen">
+        <NotificationDisplay messageEvents={messageEvents} />
+        <button
+          type="button"
+          className="bg-red-500 p-4"
+          onClick={() => {
+            void logout();
+          }}
+        >
+          Log Out
+        </button>
+        <ServerSelectBar serverList={user?.serversIn ?? []} />
+        <Outlet />
+      </div>
+    </MessageEventsProvider>
   );
 }
 export default AppScreen;
