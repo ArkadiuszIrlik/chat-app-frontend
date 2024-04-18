5cc892acf012fe325ed69608cda569074ecb04f8
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2024-04-18 21:21:49 +0200

    Refactor SignupForm component
    Refactored SignupForm to notify the user that a verification email has
    been sent to their address after the signup form has been sent
    successfully.

--- src/components/SignupForm/SignupForm.tsx
+++ src/components/SignupForm/SignupForm.tsx
@@ -1,22 +1,45 @@
 import { SubmitButtonPrimary } from '@components/form-controls';
 import TextInput from '@components/form-controls/TextInput';
 import useFetch from '@hooks/useFetch';
+import ExtendedCSSProperties from '@src/types';
 import { Formik, Form, useFormikContext } from 'formik';
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import * as Yup from 'yup';
+import EmailIcon from '@assets/mail.png';
+
+const emailIconStyles: ExtendedCSSProperties = {
+  '--mask-url': `url(${EmailIcon})`,
+};
 
 function SignupForm() {
   const [postData, setPostData] = useState({});
 
-  const { refetch, hasError, errorMessage, isLoading } = useFetch({
+  const { data, refetch, hasError, errorMessage, isLoading } = useFetch({
     initialUrl: 'auth/signup',
     method: 'POST',
     onMount: false,
     postData,
   });
 
-  return (
+  return data ? (
+    <div>
+      <h2 className="mb-2 flex items-center justify-center gap-4 text-2xl">
+        Verify your e-mail
+        <div
+          aria-hidden
+          className="alpha-mask aspect-square h-11 w-11 shrink-0 grow-0
+           bg-gradient-to-tr from-clairvoyant-900 to-cerise-600"
+          style={emailIconStyles}
+        />
+      </h2>
+      <p>
+        We&apos;ve sent a verification e-mail to the provided address. Please
+        use the link inside to confirm your e-mail address. If you can&apos;t
+        find the e-mail, make sure to check inside the “junk” folder.
+      </p>
+    </div>
+  ) : (
     <div>
       <h2 className="mb-3 text-2xl text-white">Sign Up</h2>
       <Formik
