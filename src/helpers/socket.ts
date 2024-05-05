29403ed8fbef3529daa5c5c956ecb3c87c1b2ebc
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2024-05-05 12:57:23 +0200

    Add typings for socket events

--- src/helpers/socket.ts
+++ src/helpers/socket.ts
@@ -1,7 +1,23 @@
-import { io } from 'socket.io-client';
+import { SocketEvents } from '@src/types';
+import { Socket, io } from 'socket.io-client';
 
-const socket = io(import.meta.env.VITE_SOCKET_IO_URL, {
-  autoConnect: false,
-  withCredentials: true,
-});
+interface ClientToServerEvents {
+  [SocketEvents.SendChatMessage]: (
+    message: { text: string; clientId: string },
+    socketId: string,
+  ) => void;
+}
+
+interface ServerToClientEvents {
+  [SocketEvents.ChatMessage]: (message: NetworkMessage) => void;
+}
+
+const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(
+  import.meta.env.VITE_SOCKET_IO_URL,
+  {
+    autoConnect: false,
+    withCredentials: true,
+  },
+);
+
 export default socket;
--- src/types.ts
+++ src/types.ts
@@ -3,6 +3,12 @@ import { CSSProperties } from 'react';
 type ExtendedCSSProperties = CSSProperties &
   Record<`--${string}`, number | string>;
 
+enum SocketEvents {
+  ChatMessage = 'chat message',
+  SendChatMessage = 'send chat message',
+}
+
 export {
   type ExtendedCSSProperties,
+  SocketEvents,
 };
