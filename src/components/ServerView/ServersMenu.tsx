f958f483fdd1896c09d2d5e46b5753cc92554146
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2024-06-05 21:18:25 +0200

    Refactor ServersMenu
    Fixed the styling to allow scrolling when overflow happens. Made
    onCloseMenu optional to allow for better UX when used inside of
    ChannelsScreen. Changed from padding to margin around the server list
    because it works consistently with overflow scroll. Made the border
    display above "Add a server" conditional so that it doesn't appear
    when the server list is empty. Also broke long className declarations
    into mulitple lines.

--- src/components/ServerView/ServersMenu.tsx
+++ src/components/ServerView/ServersMenu.tsx
@@ -1,4 +1,4 @@
-import { useAuth } from '@hooks/index';
+import { useServerStore } from '@hooks/index';
 import { Link, useParams } from 'react-router-dom';
 import LeftArrow from '@assets/left-arrow-icon.png';
 import PlusIcon from '@assets/plus-icon.png';
@@ -14,9 +14,14 @@ const addServerStyles: ExtendedCSSProperties = {
   '--mask-url': `url(${PlusIcon})`,
 };
 
-function ServersMenu({ onCloseMenu }: { onCloseMenu: () => void }) {
-  const { user } = useAuth()!;
-  const serverList = user?.serversIn ?? [];
+ServersMenu.defaultProps = {
+  onCloseMenu: undefined,
+};
+
+const defaultOnCloseServerMenu = () => undefined;
+
+function ServersMenu({ onCloseMenu }: { onCloseMenu?: () => void }) {
+  const { serverList } = useServerStore() ?? {};
   const { serverId } = useParams();
   const [isModalShown, setIsModalShown] = useState(false);
 
@@ -28,37 +33,54 @@ function ServersMenu({ onCloseMenu }: { onCloseMenu: () => void }) {
   }, []);
 
   return (
-    <div>
-      {isModalShown && <AddServerModal onCloseModal={handleCloseModal} />}
+    <div className="flex min-h-0 flex-col">
+      {isModalShown && (
+        <AddServerModal
+          onCloseModal={handleCloseModal}
+          onCloseServersMenu={onCloseMenu ?? defaultOnCloseServerMenu}
+        />
+      )}
       <div className="flex items-center gap-1">
-        <button
-          type="button"
-          onClick={onCloseMenu}
-          aria-label="Close servers menu"
-          className="group rounded-lg hover:bg-gray-600"
-        >
-          <div
-            className="alpha-mask aspect-square h-7 w-7 shrink-0 grow-0 bg-gray-400 group-hover:bg-gray-300"
-            style={closeMenuStyles}
-          />
-        </button>
+        {onCloseMenu && (
+          <button
+            type="button"
+            onClick={onCloseMenu}
+            aria-label="Close servers menu"
+            className="group rounded-lg hover:bg-gray-600"
+          >
+            <div
+              className="alpha-mask aspect-square h-7 w-7 shrink-0 grow-0
+               bg-gray-400 group-hover:bg-gray-300"
+              style={closeMenuStyles}
+            />
+          </button>
+        )}
         <h1 className="text-xl">Servers</h1>
       </div>
-      <div className="flex flex-col gap-1 py-2">
-        {serverList.map((server) => {
-          const isActive = server._id !== undefined && server._id === serverId;
-          return (
-            <ServerLink
-              serverId={server._id}
-              serverImg={server.serverImg}
-              serverName={server.name}
-              isActive={isActive}
-              key={server._id}
-            />
-          );
-        })}
-        <div className="flex flex-col border-t-2 border-t-gray-500 pt-1">
-          <AddServerButton onAddServer={handleAddServer} />
+      <div className="my-2 overflow-y-auto">
+        <div className="flex flex-col gap-1">
+          {serverList?.map((server) => {
+            const isActive =
+              server._id !== undefined && server._id === serverId;
+            return (
+              <ServerLink
+                serverId={server._id}
+                serverImg={server.serverImg}
+                serverName={server.name}
+                isActive={isActive}
+                key={server._id}
+              />
+            );
+          })}
+          <div
+            className={`flex flex-col ${
+              !serverList || serverList.length === 0
+                ? ''
+                : 'border-t-2 border-t-gray-500 pt-1'
+            }`}
+          >
+            <AddServerButton onAddServer={handleAddServer} />
+          </div>
         </div>
       </div>
     </div>
@@ -100,12 +122,14 @@ function AddServerButton({ onAddServer }: { onAddServer: () => void }) {
     <button
       type="button"
       onClick={onAddServer}
-      className="group flex items-center gap-2 rounded-md px-2 py-1 hover:bg-gray-600"
+      className="group flex items-center gap-2 rounded-md px-2 py-1
+       hover:bg-gray-600"
     >
       <div className="w-10">
         <div
           style={addServerStyles}
-          className="alpha-mask mx-auto aspect-square h-7 w-7 shrink-0 grow-0 bg-gray-500 group-hover:bg-green-500"
+          className="alpha-mask mx-auto aspect-square h-7 w-7 shrink-0 grow-0
+           bg-gray-500 group-hover:bg-green-500"
         />
       </div>
       <span className="truncate" title="Add server">
