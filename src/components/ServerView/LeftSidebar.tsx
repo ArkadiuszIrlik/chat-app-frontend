6f0b6e77fcff66676b3bc4e47b5b1be31ad32eb6
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2024-06-05 20:52:02 +0200

    Refactor LeftSidebar
    Refactored LeftSidebar to allow it to better display states when
    a server's channel list is loading, as well as to force the display of
    ServersMenu when it's used inside of ChannelsScreen.

--- src/components/ServerView/LeftSidebar.tsx
+++ src/components/ServerView/LeftSidebar.tsx
@@ -2,17 +2,46 @@ import { ChannelList } from '@components/ChannelList';
 import DesktopNavBar from '@components/ServerView/DesktopNavBar';
 import ServersMenu from '@components/ServerView/ServersMenu';
 import { UserBar } from '@components/UserBar';
+import styleConsts from '@constants/styleConsts';
 import { useCallback, useState } from 'react';
+import { SyncLoader } from 'react-spinners';
 
 const defaultChannelCategories: Server['channelCategories'] = [];
+
+LeftSidebar.defaultProps = {
+  isEmptyServerList: false,
+};
+
+interface LoadingProps {
+  isEmptyServerList?: false;
+  isServerLoading: true;
+  channelCategories?: Server['channelCategories'];
+  serverName?: string;
+}
+
+interface NotLoadingProps {
+  isEmptyServerList?: false;
+  isServerLoading: false;
+  channelCategories: Server['channelCategories'];
+  serverName: string;
+}
+
+interface EmptyServerListProps {
+  isEmptyServerList: true;
+  isServerLoading?: never;
+  channelCategories?: never;
+  serverName?: never;
+}
+
+type Props = LoadingProps | NotLoadingProps | EmptyServerListProps;
 
 function LeftSidebar({
   channelCategories = defaultChannelCategories,
   serverName,
 }: {
-  channelCategories: Server['channelCategories'];
-  serverName: string;
-}) {
+  isServerLoading,
+  isEmptyServerList,
+}: Props) {
   const [isServersMenuOpen, setIsServersMenuOpen] = useState(false);
 
   const handleOpenServersMenu = useCallback(() => {
@@ -22,19 +51,40 @@ function LeftSidebar({
     setIsServersMenuOpen(false);
   }, []);
 
+  function renderSwitch() {
+    switch (true) {
+      case isEmptyServerList:
+        return <ServersMenu />;
+      case isServersMenuOpen:
+        return <ServersMenu onCloseMenu={handleCloseServersMenu} />;
+      case isServerLoading:
+        return (
+          <div className="flex grow items-center justify-center">
+            <SyncLoader
+              color={styleConsts.colors.gray[300]}
+              speedMultiplier={0.8}
+              size={10}
+            />
+          </div>
+        );
+      case !isServerLoading:
+        return (
+          <ChannelList
+            channelCategories={channelCategories}
+            serverName={serverName}
+          />
+        );
+      default:
+        return null;
+    }
+  }
+
   return (
-    <div className="flex w-52 flex-col bg-gray-700 px-2 py-2">
-      {isServersMenuOpen ? (
-        <ServersMenu onCloseMenu={handleCloseServersMenu} />
-      ) : (
-        <ChannelList
-          channelCategories={channelCategories}
-          serverName={serverName}
-        />
-      )}
+    <div className="flex min-h-0 w-52 flex-col bg-gray-700 px-2 py-2">
+      {renderSwitch()}
       <div className="mt-auto flex flex-col">
         <DesktopNavBar
-          isServersMenuOpen={isServersMenuOpen}
+          isServersMenuOpen={isEmptyServerList ? true : isServersMenuOpen}
           onOpenServers={handleOpenServersMenu}
         />
         <UserBar />
