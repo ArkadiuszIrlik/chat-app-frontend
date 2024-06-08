1720b590b87a9e2587a3d63cbc72b8a568fd0e82
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2024-06-08 22:03:59 +0200

    Fix ErrorDisplay text not wrapping

--- src/components/form-controls/ErrorDisplay.tsx
+++ src/components/form-controls/ErrorDisplay.tsx
@@ -8,14 +8,16 @@ const exclamationStyles: ExtendedCSSProperties = {
 function ErrorDisplay({ errorMessage }: { errorMessage: string }) {
   return (
     <div
-      className="mb-2 flex items-center gap-3 rounded-lg border-[1px]
-             border-red-300 bg-red-900 px-3 py-2 text-red-300"
+      className="mb-2 flex
+             items-center gap-3 rounded-lg border-[1px] border-red-300
+             bg-red-900 px-3 py-2 text-red-300"
     >
       <div
         style={exclamationStyles}
-        className="alpha-mask aspect-square h-5 w-5 shrink-0 grow-0 bg-red-300"
+        className="alpha-mask aspect-square h-5 w-5 min-w-0 shrink-0 grow-0
+         bg-red-300"
       />
-      {errorMessage}
+      <div className="min-w-0 break-words">{errorMessage}</div>
     </div>
   );
 }
