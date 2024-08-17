7efa8a90bdfc35418bab32f9df0cffc00443140d
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2024-08-17 22:33:05 +0200

    Refactor ProtectedRoute component
    Because of the asynchronous nature of logging in, ProtectedRoute would
    briefly display Login page, before navigating to the authenticated
    section of the app. This was confusing and caused an unpleasant, abrupt
    flicker.
    
    The component has been refactored to make use of new state inside
    useAuth hook, which helps it understand when there is an in-flight
    login request and allowing it to display a loading page while it is
    happening, preventing the unpleasant flicker.

--- src/components/ProtectedRoute/ProtectedRoute.tsx
+++ src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -1,8 +1,32 @@
 import { useAuth } from '@hooks/index';
+import { ReactNode } from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 
 function ProtectedRoute() {
-  const { isAuthenticated } = useAuth()!;
-  return isAuthenticated ? <Outlet /> : <Navigate to="/login" replace />;
+  const { isAuthenticated, isLoggingIn, isFirstLogin } = useAuth() ?? {};
+  let returnedComponent: ReactNode;
+
+  switch (true) {
+    case isAuthenticated:
+      returnedComponent = <Outlet />;
+      break;
+    case isFirstLogin:
+    case isLoggingIn:
+      returnedComponent = <LoadingScreen />;
+      break;
+    default:
+      returnedComponent = <Navigate to="/login" replace />;
+  }
+
+  return returnedComponent;
 }
+
+function LoadingScreen() {
+  return (
+    <div className="flex min-h-screen w-full items-center justify-center">
+      Loading...
+    </div>
+  );
+}
+
 export default ProtectedRoute;
