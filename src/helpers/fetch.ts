98bc016be5e2bef1bb5b206243949ccc5c6275f0
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2024-08-17 18:09:44 +0200

    Add getClientURL fetch helper

--- src/helpers/fetch.ts
+++ src/helpers/fetch.ts
@@ -16,6 +16,24 @@ export function getURL(url: string) {
   return mainURL + nextUrl;
 }
 
+export function getClientURL(url: string) {
+  const urlExpression =
+    'https?://(www.)?[-a-zA-Z0-9@:%._+~#=]{1,256}.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)';
+  const regex = new RegExp(urlExpression);
+  if (url.match(regex)) {
+    return url;
+  }
+  let mainURL = import.meta.env.VITE_FRONTEND_URL;
+  if (mainURL === undefined) return '';
+  if (!mainURL.endsWith('/')) mainURL += '/';
+
+  let nextUrl = url;
+  if (url.length > 0 && url.startsWith('/')) {
+    nextUrl = url.substring(1, url.length);
+  }
+  return mainURL + nextUrl;
+}
+
 interface ExtendedError extends Error {
   status: number;
   data: object;
