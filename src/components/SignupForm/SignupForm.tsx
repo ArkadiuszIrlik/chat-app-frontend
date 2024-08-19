85c895f65d2726484a7ae0dae38ac1ef16ddc3ed
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2024-08-19 20:53:51 +0200

    Refactor SignupForm component

--- src/components/SignupForm/SignupForm.tsx
+++ src/components/SignupForm/SignupForm.tsx
@@ -1,12 +1,17 @@
-import { SubmitButtonPrimary } from '@components/form-controls';
+import {
+  ErrorDisplay,
+  SubmitButtonPrimary,
+  SubmittingUpdater,
+} from '@components/form-controls';
 import TextInput from '@components/form-controls/TextInput';
 import useFetch from '@hooks/useFetch';
-import ExtendedCSSProperties from '@src/types';
-import { Formik, Form, useFormikContext } from 'formik';
-import { useEffect, useState } from 'react';
+import { ExtendedCSSProperties } from '@src/types';
+import { Formik, Form } from 'formik';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import * as Yup from 'yup';
+import Yup from '@src/extendedYup';
 import EmailIcon from '@assets/mail.png';
+import { userSchema } from '@constants/validationSchema';
 
 const emailIconStyles: ExtendedCSSProperties = {
   '--mask-url': `url(${EmailIcon})`,
@@ -49,14 +54,8 @@ function SignupForm() {
           confirmPassword: '',
         }}
         validationSchema={Yup.object({
-          email: Yup.string()
-            .trim()
-            .email('Invalid email address.')
-            .required('Please enter your email.'),
-          password: Yup.string()
-            .min(8, 'Password has to be at least 8 characters long.')
-            .max(100, "Password can't be longer than 100 characters.")
-            .required('Please enter a password.'),
+          email: userSchema.email.required('Please enter your email.'),
+          password: userSchema.password.required('Please enter a password.'),
           confirmPassword: Yup.string()
             .oneOf([Yup.ref('password')], "Passwords don't match.")
             .required('Please enter your password again.'),
@@ -68,12 +67,8 @@ function SignupForm() {
       >
         <Form className="flex flex-col">
           {hasError && (
-            <div
-              className="mb-2 flex items-center gap-3 rounded-lg border-[1px]
-             border-red-300 bg-red-900 px-3 py-2 text-red-300"
-            >
-              <div className="exclamation-mask aspect-square h-5 w-5 bg-red-300" />
-              {errorMessage}
+            <div className="max-w-prose">
+              <ErrorDisplay errorMessage={errorMessage} />
             </div>
           )}
           <div>
@@ -120,17 +115,6 @@ function SignupForm() {
       </Formik>
     </div>
   );
-}
-
-function SubmittingUpdater({ isFetchLoading }: { isFetchLoading: boolean }) {
-  const { setSubmitting } = useFormikContext();
-  useEffect(() => {
-    if (!isFetchLoading) {
-      setSubmitting(false);
-    }
-  }, [isFetchLoading, setSubmitting]);
-
-  return null;
 }
 
 export default SignupForm;
