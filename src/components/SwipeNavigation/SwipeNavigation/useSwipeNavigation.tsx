db0ea8171de05227308d1b46f4fe86896bbf6a7d
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2024-10-02 20:31:56 +0200

    Add swipe direction lock to useSwipeNavigation
    Vertically scrolling the app's content was causing accidental drag
    of the adjacent columns. After experimenting with other solutions such
    as minimum drag distance, I finally settled on enforcing swipe direction
    lock. The hook determines if the user is trying to scroll vertically or
    slide horizontally and locks the rest of the swipe interaction in that
    direction. It feels responsive to use and minimizes accidental
    horizontal swipes.

--- src/components/SwipeNavigation/SwipeNavigation/useSwipeNavigation.tsx
+++ src/components/SwipeNavigation/SwipeNavigation/useSwipeNavigation.tsx
@@ -15,12 +15,19 @@ interface IOptions {
   /** Specifies minimum distance (in pixels) for a drag be
    *  registered */
   minimumDragDistance?: number;
+  /** Swipe distance (in pixels) when swiping direction is
+   * decided (vertical or horizontal) */
+  directionLockThreshold?: number;
 }
 
 function useSwipeNavigation(
   columns: ISwipeColumn[],
   swipeContainerRef: MutableRefObject<HTMLElement | null>,
-  { minimumDistance = 0, minimumDragDistance = 0 }: IOptions = {},
+  {
+    minimumDistance = 0,
+    minimumDragDistance = 0,
+    directionLockThreshold = 5,
+  }: IOptions = {},
 ) {
   const lowestIndex = 0;
   const highestIndex = columns.length - 1;
@@ -29,6 +36,9 @@ function useSwipeNavigation(
   const [swipeIndex, setSwipeIndex] = useState(mainColIndex);
   const [dragIndex, setDragIndex] = useState<number | null>(null);
   const [dragOffset, setDragOffset] = useState(0);
+  const [swipeDirection, setSwipeDirection] = useState<
+    'vertical' | 'horizontal' | null
+  >(null);
 
   // whenever columns.length changes, reset swipeIndex
   useEffect(() => {
@@ -61,52 +71,92 @@ function useSwipeNavigation(
     ({ deltaX }: { deltaX: number; deltaY: number }) => {
       setDragIndex(null);
       setDragOffset(0);
-      if (deltaX > 0 && deltaX >= minimumDistance) {
-        handleSwipeRight();
-        return;
+      if (swipeDirection === 'horizontal') {
+        if (deltaX > 0 && deltaX >= minimumDistance) {
+          handleSwipeRight();
+        }
+        if (deltaX < 0 && deltaX <= -Math.abs(minimumDistance)) {
+          handleSwipeLeft();
+        }
       }
-      if (deltaX < 0 && deltaX <= -Math.abs(minimumDistance)) {
-        handleSwipeLeft();
-      }
+      setSwipeDirection(null);
     },
-    [handleSwipeLeft, handleSwipeRight, minimumDistance],
+    [handleSwipeLeft, handleSwipeRight, minimumDistance, swipeDirection],
   );
 
   const processSwipeUpdate = useCallback(
-    ({ deltaX }: { deltaX: number; deltaY: number }) => {
-      setDragOffset(Math.round(deltaX));
-      if (Math.abs(deltaX) < minimumDragDistance) {
-        setDragIndex(null);
-        return;
+    ({
+      deltaX,
+      deltaY,
+      e,
+    }: {
+      deltaX: number;
+      deltaY: number;
+      e: MouseEvent | TouchEvent;
+    }) => {
+      if (!swipeDirection) {
+        if (e.cancelable) {
+          e.preventDefault();
+        }
+        if (
+          Math.abs(deltaX) >= directionLockThreshold ||
+          Math.abs(deltaY) >= directionLockThreshold
+        ) {
+          // soft preference for vertical direction
+          if (Math.abs(deltaY) >= Math.abs(deltaX)) {
+            setSwipeDirection('vertical');
+          } else {
+            setSwipeDirection('horizontal');
+          }
+        } else {
+          return;
+        }
       }
-      if (swipeIndex === mainColIndex) {
-        if (deltaX > 0) {
-          setDragIndex(swipeIndex - 1);
+
+      if (swipeDirection === 'horizontal') {
+        if (e.cancelable) {
+          e.preventDefault();
         }
-        if (deltaX < 0) {
-          setDragIndex(swipeIndex + 1);
+        setDragOffset(Math.round(deltaX));
+        if (Math.abs(deltaX) < minimumDragDistance) {
+          setDragIndex(null);
+          return;
         }
-        return;
-      }
-      if (swipeIndex < mainColIndex) {
-        if (deltaX > 0) {
-          setDragIndex(swipeIndex - 1);
+        if (swipeIndex === mainColIndex) {
+          if (deltaX > 0) {
+            setDragIndex(swipeIndex - 1);
+          }
+          if (deltaX < 0) {
+            setDragIndex(swipeIndex + 1);
+          }
+          return;
         }
-        if (deltaX < 0) {
-          setDragIndex(swipeIndex);
+        if (swipeIndex < mainColIndex) {
+          if (deltaX > 0) {
+            setDragIndex(swipeIndex - 1);
+          }
+          if (deltaX < 0) {
+            setDragIndex(swipeIndex);
+          }
+          return;
         }
-        return;
-      }
-      if (swipeIndex > mainColIndex) {
-        if (deltaX > 0) {
-          setDragIndex(swipeIndex);
-        }
-        if (deltaX < 0) {
-          setDragIndex(swipeIndex + 1);
+        if (swipeIndex > mainColIndex) {
+          if (deltaX > 0) {
+            setDragIndex(swipeIndex);
+          }
+          if (deltaX < 0) {
+            setDragIndex(swipeIndex + 1);
+          }
         }
       }
     },
-    [swipeIndex, mainColIndex, minimumDragDistance],
+    [
+      swipeIndex,
+      mainColIndex,
+      minimumDragDistance,
+      directionLockThreshold,
+      swipeDirection,
+    ],
   );
 
   useSwipe(processSwipe, processSwipeUpdate, {
