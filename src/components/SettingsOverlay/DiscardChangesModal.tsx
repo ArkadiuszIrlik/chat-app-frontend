1ea685674a88ce9d917a652645bc912d65a38003
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2024-10-18 16:32:12 +0200

    Add reponsive styling to SettingsOverlay

--- src/components/SettingsOverlay/DiscardChangesModal.tsx
+++ src/components/SettingsOverlay/DiscardChangesModal.tsx
@@ -11,16 +11,16 @@ function DiscardChangesModal({
   return (
     <ModalOverlay>
       <div
-        className="fixed left-1/2 top-1/2 w-96 -translate-x-1/2
+        className="fixed left-1/2 top-1/2 w-11/12 max-w-96 -translate-x-1/2
        -translate-y-1/2 rounded-md bg-gray-800 px-5 py-4"
       >
         <div>
           <h3 className="mb-1 text-xl text-gray-300">Discard Changes</h3>
-          <p className="mb-4">
+          <p className="mb-4 text-gray-200">
             You have unsaved changes. Are you sure you want to leave the page
             and discard all changes?
           </p>
-          <div className="flex items-center justify-center gap-10">
+          <div className="flex items-center justify-start gap-10">
             <button
               type="button"
               onClick={onCancel}
--- src/components/SettingsOverlay/SettingsOverlay.tsx
+++ src/components/SettingsOverlay/SettingsOverlay.tsx
@@ -4,9 +4,11 @@ import CloseIcon from '@assets/close-icon.png';
 import { ReactNode, useCallback, useEffect, useState } from 'react';
 import { useFormikContext } from 'formik';
 import deepEqual from 'deep-equal';
-import { useDebounce } from '@uidotdev/usehooks';
+import { useDebounce, useMediaQuery } from '@uidotdev/usehooks';
 import DiscardChangesModal from '@components/SettingsOverlay/DiscardChangesModal';
 import { PrimaryButton } from '@components/PrimaryButton';
+import { createPortal } from 'react-dom';
+import styleConsts from '@constants/styleConsts';
 
 const closeButtonStylesObj: ExtendedCSSProperties = {
   '--mask-url': `url(${CloseIcon})`,
@@ -70,57 +72,76 @@ function SettingsOverlay({
     }
   }, [blocker]);
 
-  return (
-    <div className="fixed inset-0 z-40 flex grow">
-      <div className="w-52 bg-gray-700 px-2 py-2">
-        <h1 className="mb-2 text-xl text-gray-200">{label}</h1>
-      </div>
-      <div className="grow bg-gray-700">
-        <div className="px-4 py-3">
-          <button
-            type="button"
-            onClick={handleCloseSettings}
-            aria-label={`Close ${label}`}
-            className="group ml-auto block rounded-md p-1 hover:bg-gray-600"
-          >
-            <div
-              className="alpha-mask aspect-square h-5 w-5 shrink-0 grow-0 bg-gray-400
-              group-hover:bg-gray-300"
-              style={closeButtonStylesObj}
-            />
-          </button>
-        </div>
-        <div>{children}</div>
-        {(blocker.state === 'blocked' || isBlocked) && (
-          <DiscardChangesModal
-            onProceed={handleProceedDiscard}
-            onCancel={handleCancelDiscard}
-          />
-        )}
-        {haveValuesChanged && (
-          <div
-            className="fixed bottom-10 left-1/2 flex -translate-x-1/2
-         items-center justify-between gap-44 rounded-lg bg-gray-800 px-6 py-3"
-          >
-            <button
-              type="button"
-              className="block underline-offset-2 hover:underline"
-              onClick={handleCancelChanges}
-            >
-              Cancel&nbsp;changes
-            </button>
-            <PrimaryButton
-              type="button"
-              disabled={isSubmitting}
-              // eslint-disable-next-line @typescript-eslint/no-misused-promises
-              onClickHandler={submitForm}
-            >
-              Save&nbsp;changes
-            </PrimaryButton>
+  const isExtraSmallScreen = useMediaQuery(
+    `only screen and (min-width: ${styleConsts.screens.xs}`,
+  );
+
+  return createPortal(
+    <div className="fixed inset-0 z-40 bg-gray-700">
+      <div
+        className="mx-auto flex lg:mx-[8%] xl:mx-[14%] 2xl:mx-auto
+       2xl:max-w-[1800px]"
+      >
+        {isExtraSmallScreen && (
+          <div className="w-52 bg-gray-700 px-2 py-2">
+            <h1 className="mb-2 text-xl text-gray-200">{label}</h1>
           </div>
         )}
+        <div className="grow bg-gray-700">
+          <div className="flex px-4 py-3">
+            {!isExtraSmallScreen && (
+              <h1 className="mb-2 text-xl text-gray-200">{label}</h1>
+            )}
+            <button
+              type="button"
+              onClick={handleCloseSettings}
+              aria-label={`Close ${label}`}
+              className="group ml-auto block rounded-md p-1 hover:bg-gray-600"
+            >
+              <div
+                className="alpha-mask aspect-square h-5 w-5 shrink-0 grow-0 bg-gray-400
+              group-hover:bg-gray-300"
+                style={closeButtonStylesObj}
+              />
+            </button>
+          </div>
+          <div className="px-3 xs:px-0">{children}</div>
+          {(blocker.state === 'blocked' || isBlocked) && (
+            <DiscardChangesModal
+              onProceed={handleProceedDiscard}
+              onCancel={handleCancelDiscard}
+            />
+          )}
+          {haveValuesChanged && (
+            <div
+              className="fixed bottom-10 left-1/2 flex w-11/12 -translate-x-1/2
+             flex-wrap items-center justify-center gap-x-10 gap-y-8 rounded-lg
+              bg-gray-800 px-6 py-3 xs:w-auto xs:flex-nowrap xs:justify-between
+               xs:gap-20 sm:gap-44"
+            >
+              <button
+                type="button"
+                className="block underline-offset-2 hover:underline"
+                onClick={handleCancelChanges}
+              >
+                Cancel&nbsp;changes
+              </button>
+              <div>
+                <PrimaryButton
+                  type="button"
+                  disabled={isSubmitting}
+                  // eslint-disable-next-line @typescript-eslint/no-misused-promises
+                  onClickHandler={submitForm}
+                >
+                  Save&nbsp;changes
+                </PrimaryButton>
+              </div>
+            </div>
+          )}
+        </div>
       </div>
-    </div>
+    </div>,
+    document.body,
   );
 }
 export default SettingsOverlay;
