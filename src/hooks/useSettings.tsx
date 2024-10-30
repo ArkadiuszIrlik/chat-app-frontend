9e7a6d3dfc444e4ed1d031b20648d403a49a3814
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2024-10-30 20:26:00 +0100

    Refactor useSettings hook
    Fixed a bug where settings read from localStorage didn't get applied.
    Refactored the type guard logic. Simplified setting object type and
    exported it. Refactored settings state so that it gets correctly
    initialized with the user preferences stored in localStorage, preventing
    a situation where the user is shown the default value first, before
    the useEffect runs.

--- src/hooks/useSettings.tsx
+++ src/hooks/useSettings.tsx
@@ -9,16 +9,6 @@ import {
   useState,
 } from 'react';
 
-interface BaseSettingProperties {
-  uiName: string;
-  version: number;
-}
-type SettingObject = BaseSettingProperties &
-  (
-    | { type: SettingTypes.Check; value: boolean }
-    | { type: SettingTypes.Range; value: number }
-  );
-
 const defaultSettings = {
   ENABLE_NOTIFICATION_SOUND: {
     type: SettingTypes.Check as const,
@@ -35,43 +25,15 @@ const defaultSettings = {
 };
 
 export type SettingsKey = keyof typeof defaultSettings;
+export type Settings = typeof defaultSettings;
 
-const nextSettings = { ...defaultSettings };
+function isPlainObject(obj: unknown): obj is Record<PropertyKey, unknown> {
+  return typeof obj === 'object' && !Array.isArray(obj) && obj !== null;
+}
 
 function useSettings() {
-  const [settings, setSettings] = useState(nextSettings);
+  const [settings, setSettings] = useState(getUserSettings());
   const debouncedSettings = useDebounce(settings, 500);
-
-  useEffect(() => {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    const parsedLocalSettings = JSON.parse(
-      localStorage.getItem('userPreferences') ?? '{}',
-    );
-
-    // typescript struggles to correctly narrow types without performing
-    // expensive and excessive type checks, hence the need for type
-    // assertions and ignoring eslint rules. Regardless of that, the
-    // code performs all the checks necessary to ensure type safety
-    if (typeof parsedLocalSettings === 'object') {
-      Object.entries(parsedLocalSettings as object).forEach(([key, v]) => {
-        const nextSettingObject = nextSettings[key as SettingsKey] as
-          | SettingObject
-          | undefined;
-        if (nextSettingObject) {
-          if (typeof v === 'object' && 'version' in v && 'value' in v) {
-            v as object;
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-            if (v.version === nextSettingObject.version) {
-              // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-              if (typeof v.value === typeof nextSettingObject.value)
-                // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
-                nextSettingObject.value = v.value;
-            }
-          }
-        }
-      });
-    }
-  }, []);
 
   useEffect(() => {
     localStorage.setItem('userPreferences', JSON.stringify(debouncedSettings));
@@ -133,3 +95,31 @@ function SettingsConsumer() {
 }
 
 export default SettingsConsumer;
+
+function getUserSettings() {
+  const nextSettings = Object.fromEntries(
+    Object.entries(defaultSettings),
+  ) as Settings;
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const parsedLocalSettings = JSON.parse(
+    localStorage.getItem('userPreferences') ?? '{}',
+  );
+
+  if (isPlainObject(parsedLocalSettings)) {
+    Object.entries(nextSettings).forEach(([settingKey, currentSetting]) => {
+      const localStoreSetting = parsedLocalSettings[settingKey];
+      if (isPlainObject(localStoreSetting)) {
+        if (localStoreSetting.version === currentSetting.version) {
+          if (typeof localStoreSetting.value === typeof currentSetting.value) {
+            // @ts-expect-error Type check done above
+            // eslint-disable-next-line no-param-reassign
+            currentSetting.value = localStoreSetting.value;
+          }
+        }
+      }
+    });
+  }
+
+  return nextSettings;
+}
