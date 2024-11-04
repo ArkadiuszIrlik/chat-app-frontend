1a11ae4cd7e874b87ddad2294adea6c0f75e8f8d
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2024-11-04 21:37:51 +0100

    Change MessageInput styling
    Added max height declaration so the text editor doesn't keep growing
    indefinitely as more text is added. Made other small adjustments to
    styles.

--- src/components/ChatDisplay/MessageInput.tsx
+++ src/components/ChatDisplay/MessageInput.tsx
@@ -11,6 +11,7 @@ const urlRegex =
 
 function MessageInput({ channelSocketId }: { channelSocketId: string }) {
   const [isSubmitting, setIsSubmitting] = useState(false);
+  const [isEditorCreated, setIsEditorCreated] = useState(false);
 
   const ShortcutExtension = useMemo(
     () =>
@@ -35,34 +36,47 @@ function MessageInput({ channelSocketId }: { channelSocketId: string }) {
     setIsSubmitting(false);
   }, [setIsSubmitting]);
   return (
-    <div className="px-4 py-2">
-      <EditorProvider
-        extensions={[
-          StarterKit,
-          Placeholder.configure({
-            placeholder: 'Say something...',
-          }),
-          ShortcutExtension,
-        ]}
-        editorProps={{
-          attributes: {
-            class:
-              'prose prose-invert bg-gray-600 mx-auto px-5 py-2 rounded-lg',
-          },
-          transformPastedHTML(html) {
-            return (
-              new DOMParser().parseFromString(html, 'text/html').documentElement
-                .textContent ?? ''
-            );
-          },
-        }}
+    <div className="min-h-10 px-3 py-2 opacity-100">
+      <div
+        className={`z-0 mx-auto max-w-prose rounded-lg bg-gray-600 px-2 py-2 has-[:focus-visible]:outline ${
+          // mitigates flash before editor is loaded
+          isEditorCreated ? '' : 'min-h-10'
+        }`}
       >
-        <MessageSender
-          channelSocketId={channelSocketId}
-          isSubmitting={isSubmitting}
-          onStopSubmitting={handleStopSubmitting}
-        />
-      </EditorProvider>
+        <div className="relative">
+          <div className="absolute left-0 right-0 top-0 z-10 h-2 bg-gradient-to-t from-gray-600/0 to-gray-600" />
+        </div>
+        <EditorProvider
+          onCreate={() => {
+            setIsEditorCreated(true);
+          }}
+          extensions={[
+            StarterKit,
+            Placeholder.configure({
+              placeholder: 'Say something...',
+            }),
+            ShortcutExtension,
+          ]}
+          editorProps={{
+            attributes: {
+              class:
+                'prose prose-invert px-2 xs:px-5 max-h-28 overflow-auto overscroll-contain focus-visible:outline-none',
+            },
+            transformPastedHTML(html) {
+              return (
+                new DOMParser().parseFromString(html, 'text/html')
+                  .documentElement.textContent ?? ''
+              );
+            },
+          }}
+        >
+          <MessageSender
+            channelSocketId={channelSocketId}
+            isSubmitting={isSubmitting}
+            onStopSubmitting={handleStopSubmitting}
+          />
+        </EditorProvider>
+      </div>
     </div>
   );
 }
