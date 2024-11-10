938f3194478f976f91fbdd0bdaed6f5d898e7005
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2024-11-10 19:33:38 +0100

    Refactor useChatMessages hook
    Updated the hook to reflect the new API endpoint - changed the URL,
    added support for limit and cursor query params. Added an exported
    function that allows to easily load more messages for a given chatId.
    Added hasFirstMessage state which reflects the previousCursor value
    returned by the API. Refactored some of the existing code for returning
    messages array.

--- src/hooks/useChatMessages.ts
+++ src/hooks/useChatMessages.ts
@@ -1,24 +1,52 @@
 import { parseNetworkMessages } from '@helpers/data';
 import { genericFetcherCredentials, HttpError } from '@helpers/fetch';
+import {
+  CursorValue,
+  useMessageCursorStore,
+  useMessageStore,
+  useSocket,
+} from '@hooks/index';
 import { SocketEvents } from '@src/types';
-import { useCallback, useEffect } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import useSWR from 'swr';
+
+const defaultLimit = 20;
+const defaultCursor = undefined;
 
 function useChatMessages(chatId: string | undefined) {
   const { socket, sendChatMessage: sendMessageToSocket } = useSocket() ?? {};
   const { messageStore, addToStore } = useMessageStore() ?? {};
+  const { messageCursorStore, getPreviousCursor, updatePreviousCursor } =
+    useMessageCursorStore() ?? {};
+  const [shouldFetch, setShouldFetch] = useState(false);
+  const [limit, setLimit] = useState(defaultLimit);
+  const [previousCursor, setPreviousCursor] = useState<CursorValue | undefined>(
+    defaultCursor,
+  );
 
-  let messages: Message[] = [];
-  let shouldFetch = false;
-
-  if (chatId !== undefined) {
-    const storedMessages = messageStore ? messageStore[chatId] : null;
-    if (storedMessages) {
-      messages = storedMessages;
-    } else {
-      shouldFetch = true;
+  const [hasFirstMessage, setHasFirstMessage] = useState(false);
+  useEffect(() => {
+    if (getPreviousCursor && chatId) {
+      const currentChatCursor = getPreviousCursor(chatId);
+      if (currentChatCursor === null) {
+        setHasFirstMessage(true);
+      } else {
+        setHasFirstMessage(false);
+      }
     }
+  }, [messageCursorStore, getPreviousCursor, chatId]);
+
+  let messages: Message[] | undefined;
+  if (chatId !== undefined && messageStore) {
+    messages = messageStore[chatId];
   }
+
+  // initiate fetch when no messages
+  useEffect(() => {
+    if (!messages) {
+      setShouldFetch(true);
+    }
+  }, [messages]);
 
   // add incoming socket ChatMessage events to store
   useEffect(() => {
@@ -37,18 +65,53 @@ function useChatMessages(chatId: string | undefined) {
   }, [socket, addToStore]);
 
   // fetch chat messages from API
+  const messageUrl = getMessageUrl(chatId ?? '', limit, previousCursor);
   const { data, error, isLoading } = useSWR<
-    { message: string; data: { messages: NetworkMessage[] } },
+    {
+      message: string;
+      data: { messages: NetworkMessage[]; previousCursor: number | null };
+    },
     HttpError
-  >(() => (shouldFetch ? `/chat/${chatId}` : null), genericFetcherCredentials);
+  >(() => (shouldFetch ? messageUrl : null), genericFetcherCredentials);
 
+  // reset values after successful fetch
+  useEffect(() => {
+    if (data ?? (error && error.data.message === 'Messages not found')) {
+      setShouldFetch(false);
+      setLimit(defaultLimit);
+      setPreviousCursor(defaultCursor);
+    }
+  }, [data, error]);
+
+  // process fetch results
   useEffect(() => {
     if (shouldFetch && !isLoading && (data ?? error) && chatId && addToStore) {
-      const networkMessages = data?.data?.messages ?? [];
-      const parsedMessages = parseNetworkMessages(networkMessages);
+      let nextMessages: NetworkMessage[];
+      let nextPreviousCursor: CursorValue;
+      if (data) {
+        nextMessages = data.data.messages;
+        nextPreviousCursor = data.data.previousCursor;
+      } else if (error && error.data.message === 'Messages not found') {
+        nextMessages = [];
+        nextPreviousCursor = null;
+      } else {
+        return;
+      }
+      const parsedMessages = parseNetworkMessages(nextMessages);
       addToStore(chatId, parsedMessages);
+      if (updatePreviousCursor) {
+        updatePreviousCursor(chatId, nextPreviousCursor);
+      }
     }
-  }, [data, isLoading, error, shouldFetch, addToStore, chatId]);
+  }, [
+    data,
+    isLoading,
+    error,
+    shouldFetch,
+    addToStore,
+    chatId,
+    updatePreviousCursor,
+  ]);
 
   const sendMessage = useCallback(
     (
@@ -63,7 +126,42 @@ function useChatMessages(chatId: string | undefined) {
     [addToStore, sendMessageToSocket],
   );
 
-  return { messages, isLoading, error, sendMessage };
+  const loadMoreMessages = useCallback(
+    (messageCount = defaultLimit) => {
+      let nextPreviousCursor;
+      if (getPreviousCursor && chatId) {
+        nextPreviousCursor = getPreviousCursor(chatId);
+      }
+      // cancel if nothing more to fetch
+      if (nextPreviousCursor === null) {
+        return;
+      }
+      setLimit(messageCount);
+      setPreviousCursor(nextPreviousCursor);
+      setShouldFetch(true);
+    },
+    [chatId, getPreviousCursor],
+  );
+
+  return {
+    messages,
+    hasFirstMessage,
+    isLoading,
+    error,
+    sendMessage,
+    loadMoreMessages,
+  };
+}
+
+function getMessageUrl(chatId: string, limit: number, cursor?: CursorValue) {
+  const dummyBaseUrl = 'https://example.com';
+  const messageUrl = new URL(`/chat/${chatId}/messages`, dummyBaseUrl);
+  messageUrl.searchParams.set('limit', limit.toString());
+  if (cursor !== undefined) {
+    messageUrl.searchParams.set('cursor', String(cursor));
+  }
+
+  return messageUrl.pathname + messageUrl.search;
 }
 
 export default useChatMessages;
