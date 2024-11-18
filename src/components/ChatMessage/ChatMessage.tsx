92f834f1b017171a373efaa101b1aed7f36b3251
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2024-11-18 18:30:10 +0100

    Change text wrapping on long names in ChatMessage
    When there's no room to display the full name, the date display element
    will now wrap to the next line, in order to prioritize showing as much
    of the name as possible.

--- src/components/ChatMessage/ChatMessage.tsx
+++ src/components/ChatMessage/ChatMessage.tsx
@@ -26,12 +26,14 @@ function ChatMessage({
       </div>
       <div className="flex min-w-0 flex-col">
         <div className="flex items-baseline gap-2">
-          <span className="min-w-10 shrink truncate text-blue-400">
-            {authorName}
-          </span>
-          <span className="min-w-14 text-right text-xs text-gray-200">
-            {formatDate(postedAt)}
-          </span>
+          <div className="flex min-w-10 flex-wrap items-baseline gap-x-2">
+            <span className="min-w-10 shrink truncate text-blue-400">
+              {authorName}
+            </span>
+            <span className="min-w-14 text-xs text-gray-200">
+              {formatDate(postedAt)}
+            </span>
+          </div>
         </div>
         <MessageBody text={messageText} />
         <MessageImageContainer messageText={messageText} />
