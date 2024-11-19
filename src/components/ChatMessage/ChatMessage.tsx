9c46bd5298fe7576b4322744fdc7f4c6fd0cc155
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2024-11-19 17:32:12 +0100

    Refactor ChatMessage display
    Extracted the purely presentational part of ChatMessage into a
    separate MessageDisplay component, which allows for messages to be
    displayed in other places around the app with consistent presentation,
    while not using all the business logic of ChatMessage component.

--- src/components/ChatMessage/ChatMessage.tsx
+++ src/components/ChatMessage/ChatMessage.tsx
@@ -1,7 +1,4 @@
-import formatDate from '@components/ChatMessage/ChatMessage.helpers';
-import MessageBody from '@components/ChatMessage/MessageBody';
-import MessageImageContainer from '@components/ChatMessage/MessageImageContainer';
-import { UserProfileImage } from '@components/UserProfileImage';
+import MessageDisplay from '@components/ChatMessage/MessageDisplay';
 
 function ChatMessage({
   authorName,
@@ -17,28 +14,13 @@ function ChatMessage({
   scrollOffset: unknown;
 }) {
   return (
-    <div
-      className="flex items-start gap-4 rounded-md bg-gray-700 px-3 py-2"
-      data-scroll-offset={scrollOffset}
-    >
-      <div className="aspect-square h-10 w-10 shrink-0 grow-0">
-        <UserProfileImage image={authorImg} />
-      </div>
-      <div className="flex min-w-0 flex-col">
-        <div className="flex items-baseline gap-2">
-          <div className="flex min-w-10 flex-wrap items-baseline gap-x-2">
-            <span className="min-w-10 shrink truncate text-blue-400">
-              {authorName}
-            </span>
-            <span className="min-w-14 text-xs text-gray-200">
-              {formatDate(postedAt)}
-            </span>
-          </div>
-        </div>
-        <MessageBody text={messageText} />
-        <MessageImageContainer messageText={messageText} />
-      </div>
-    </div>
+    <MessageDisplay
+      authorImg={authorImg}
+      authorName={authorName}
+      messageText={messageText}
+      postedAt={postedAt}
+      scrollOffset={scrollOffset}
+    />
   );
 }
 export default ChatMessage;
---
+++ src/components/ChatMessage/MessageDisplay.tsx
@@ -0,0 +1,55 @@
+import formatDate from '@components/ChatMessage/ChatMessage.helpers';
+import MessageBody from '@components/ChatMessage/MessageBody';
+import MessageImageContainer from '@components/ChatMessage/MessageImageContainer';
+import { UserProfileImage } from '@components/UserProfileImage';
+import { ReactNode } from 'react';
+
+MessageDisplay.defaultProps = {
+  scrollOffset: undefined,
+  headerSlot: null,
+};
+
+function MessageDisplay({
+  authorName,
+  authorImg,
+  messageText,
+  postedAt,
+  scrollOffset = undefined,
+  headerSlot = null,
+}: {
+  authorName: string;
+  authorImg: string;
+  messageText: string;
+  postedAt: Date;
+  scrollOffset?: unknown;
+  headerSlot?: ReactNode;
+}) {
+  return (
+    <div
+      // "group" can be used as needed by elements in the headerSlot
+      className="group/message-container flex items-start gap-4 rounded-md
+       bg-gray-700 px-3 py-2"
+      data-scroll-offset={scrollOffset}
+    >
+      <div className="aspect-square h-10 w-10 shrink-0 grow-0">
+        <UserProfileImage image={authorImg} />
+      </div>
+      <div className="flex w-full min-w-0 max-w-prose flex-col">
+        <div className="flex items-baseline gap-2">
+          <div className="flex min-w-10 flex-wrap items-baseline gap-x-2">
+            <span className="min-w-10 shrink truncate text-blue-400">
+              {authorName}
+            </span>
+            <span className="min-w-14 text-xs text-gray-200">
+              {formatDate(postedAt)}
+            </span>
+          </div>
+          {headerSlot}
+        </div>
+        <MessageBody text={messageText} />
+        <MessageImageContainer messageText={messageText} />
+      </div>
+    </div>
+  );
+}
+export default MessageDisplay;
