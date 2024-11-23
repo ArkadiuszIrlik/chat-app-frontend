c823a6184b125e7885cf9843210de88df5e8801c
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2024-11-23 16:19:37 +0100

    Add conditional option display to MessagePanel
    MessagePanel will now only display the options passed into the
    "optionsToDisplay" param. This gives the parent component information
    about how many options there are to display and if there aren't any,
    it can choose to not render this component and any buttons that open
    the panel.

--- src/components/ChatMessage/MessagePanel.tsx
+++ src/components/ChatMessage/MessagePanel.tsx
@@ -1,15 +1,15 @@
 import { ExtendedCSSProperties } from '@src/types';
 import TrashIcon from '@assets/trash-icon.png';
-import { useEffect, useState } from 'react';
+import { ReactNode, useEffect, useState } from 'react';
 import DeleteMessageModal from '@components/ChatMessage/DeleteMessageModal';
+import {
+  Option,
+  PanelOptionsIds,
+} from '@components/ChatMessage/MessagePanel.types';
 
 const removeStyle: ExtendedCSSProperties = {
   '--mask-url': `url(${TrashIcon})`,
 };
-
-enum PanelOptionsIds {
-  DeleteMessage = 'delete_message',
-}
 
 function MessagePanel({
   chatId,
@@ -18,6 +18,7 @@ function MessagePanel({
   authorImg,
   messageText,
   postedAt,
+  optionsToDisplay,
   onChangeActiveOption,
 }: {
   chatId: string;
@@ -26,6 +27,7 @@ function MessagePanel({
   authorImg: string;
   messageText: string;
   postedAt: Date;
+  optionsToDisplay: Option[];
   onChangeActiveOption: (activeOption: PanelOptionsIds | null) => void;
 }) {
   const [activeOptionId, setActiveOptionId] = useState<PanelOptionsIds | null>(
@@ -40,32 +42,53 @@ function MessagePanel({
       className="absolute right-0 top-0 min-h-10 min-w-48 max-w-72
        overscroll-contain rounded-lg bg-gray-800 p-2"
     >
-      <button
-        type="button"
-        onClick={() => {
-          setActiveOptionId(PanelOptionsIds.DeleteMessage);
-        }}
-        className="group/delete flex w-full items-center gap-4 rounded-lg
-         px-2 py-1 using-mouse:hover:bg-gray-700"
+      <DisplayController
+        optionsToDisplay={optionsToDisplay}
+        optionId={PanelOptionsIds.DeleteMessage}
       >
-        <div
-          className="alpha-mask aspect-square h-4 w-4 shrink-0 grow-0 bg-red-600"
-          style={removeStyle}
+        <button
+          type="button"
+          onClick={() => {
+            setActiveOptionId(PanelOptionsIds.DeleteMessage);
+          }}
+          className="group/delete flex w-full items-center gap-4 rounded-lg
+           px-2 py-1 using-mouse:hover:bg-gray-700"
+        >
+          <div
+            className="alpha-mask aspect-square h-4 w-4 shrink-0 grow-0 bg-red-600"
+            style={removeStyle}
+          />
+          <span className="min-w-0 text-red-300">Delete Message</span>
+        </button>
+        <DeleteMessageModal
+          isOpen={activeOptionId === PanelOptionsIds.DeleteMessage}
+          chatId={chatId}
+          messageId={messageId}
+          authorName={authorName}
+          authorImg={authorImg}
+          messageText={messageText}
+          postedAt={postedAt}
+          onCloseModal={() => setActiveOptionId(null)}
         />
-        <span className="min-w-0 text-red-300">Delete Message</span>
-      </button>
-      <DeleteMessageModal
-        isOpen={activeOptionId === PanelOptionsIds.DeleteMessage}
-        chatId={chatId}
-        messageId={messageId}
-        authorName={authorName}
-        authorImg={authorImg}
-        messageText={messageText}
-        postedAt={postedAt}
-        onCloseModal={() => setActiveOptionId(null)}
-      />
+      </DisplayController>
     </div>
   );
+}
+
+function DisplayController({
+  optionsToDisplay,
+  optionId,
+  children,
+}: {
+  optionsToDisplay: Option[];
+  optionId: PanelOptionsIds;
+  children: ReactNode;
+}) {
+  const shouldDisplay = !!optionsToDisplay.find(
+    (option) => option.optionId === optionId,
+  );
+
+  return shouldDisplay ? children : null;
 }
 
 export default MessagePanel;
