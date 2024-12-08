58b2018f03744ed79f3b3c0ccfc4c9f0d9f27d55
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2024-12-08 13:16:01 +0100

    Refactor ChannelPanelModal
    Updated the way user navigates to the new ChannelSettings page. Replaced
    custom ModalButton with the reusable one imported from Modal components.

--- src/components/ChannelButton/ChannelPanelModal.tsx
+++ src/components/ChannelButton/ChannelPanelModal.tsx
@@ -2,10 +2,10 @@ import SettingsIcon from '@assets/settings-icon.png';
 import { ExtendedCSSProperties } from '@src/types';
 import TextChannelIcon from '@assets/text-channel-icon.png';
 import VoiceChannelIcon from '@assets/voice-channel-icon.png';
-import { useCallback, useState } from 'react';
-import { ChannelSettings } from '@components/ChannelSettings';
 import { ModalContainer } from '@components/ModalContainer';
 import { CloseButton } from '@components/CloseButton';
+import { ModalButtonA } from '@components/Modal';
+import { useNavigate } from 'react-router-dom';
 
 const textChannelStyle: ExtendedCSSProperties = {
   '--mask-url': `url(${TextChannelIcon})`,
@@ -51,7 +51,6 @@ function ChannelPanelModal({
     </ModalContainer>
   );
 }
-export default ChannelPanelModal;
 
 function ModalContent({
   server,
@@ -66,8 +65,7 @@ function ModalContent({
   channelType: 'text' | 'voice';
   onCloseModal: () => void;
 }) {
-  const { isChannelSettingsOpen, openSettings, closeSettings } =
-    useChannelSettings();
+  const navigate = useNavigate();
 
   return (
     <div>
@@ -87,77 +85,19 @@ function ModalContent({
         />
       </div>
       <div className="mx-4 flex flex-col gap-2">
-        <ModalButton
+        <ModalButtonA
           text="Channel Settings"
           ariaLabel="Open channel settings"
           iconStyleObj={settingsIconStyle}
-          onClick={openSettings}
+          onClick={() => {
+            navigate(`/app/channel-settings/${server._id}/${channel._id}`, {
+              state: { channel, server },
+            });
+          }}
         />
       </div>
-      {isChannelSettingsOpen && (
-        <ChannelSettings
-          server={server}
-          channel={channel}
-          onCloseSettings={closeSettings}
-        />
-      )}
     </div>
   );
 }
 
-ModalButton.defaultProps = {
-  ariaLabel: '',
-};
-
-function ModalButton({
-  text,
-  iconStyleObj,
-  ariaLabel = '',
-  onClick,
-}: {
-  text: string;
-  iconStyleObj: ExtendedCSSProperties;
-  ariaLabel?: string;
-  onClick: () => void;
-}) {
-  return (
-    <button
-      type="button"
-      onClick={onClick}
-      aria-label={ariaLabel || undefined}
-      className="group flex w-full items-center gap-4 rounded-md bg-gray-600
-      px-2 py-2 text-gray-300 active:bg-gray-500 active:text-gray-200
-      using-mouse:hover:bg-gray-500 using-mouse:hover:text-gray-200"
-    >
-      <div
-        className="alpha-mask mx-1 aspect-square h-5 w-5 bg-gray-400 
-        group-active:bg-gray-300 using-mouse:group-hover:bg-gray-300"
-        style={iconStyleObj}
-      />
-      {text}
-    </button>
-  );
-}
-
-function useChannelSettings() {
-  const [isChannelSettingsOpen, setIsChannelSettingsOpen] = useState(false);
-
-  const openSettings = useCallback(() => {
-    setIsChannelSettingsOpen(true);
-  }, []);
-
-  const closeSettings = useCallback(() => {
-    setIsChannelSettingsOpen(false);
-  }, []);
-
-  const toggleSettingsOpen = useCallback(() => {
-    setIsChannelSettingsOpen((isOpen) => !isOpen);
-  }, []);
-
-  return {
-    isChannelSettingsOpen,
-    openSettings,
-    closeSettings,
-    toggleSettingsOpen,
-  };
-}
+export default ChannelPanelModal;
