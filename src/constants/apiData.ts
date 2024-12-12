514fb444699c1c21d4e7562f7bf2cdd8e8c7973b
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2024-12-12 21:21:18 +0100

    Add apiData constants
    Added data constants specifying api-supported file formats for user
    submitted images. Both server and profile image have a dedicated
    constant with a human readable string that can be displayed to the user
    in e.g. error messages.

--- src/constants/apiData.ts
+++ src/constants/apiData.ts
@@ -2,6 +2,8 @@ import { getClientURL } from '@helpers/fetch';
 
 const serverInviteRegex = /(?:invite\/)([a-zA-Z0-9]{10})$|^([a-zA-Z0-9]{10})$/;
 const exampleServerInviteLink = getClientURL('app/invite/M0DQ26NLE7');
+// human-readable string listing the allowed formats
+const SUPPORTED_PROFILE_IMG_FORMATS_HUMAN = '.jpg, .png, .webp, .svg, .avif';
 const SUPPORTED_PROFILE_IMG_MIME_TYPES = [
   'image/jpeg',
   'image/avif',
@@ -9,9 +11,14 @@ const SUPPORTED_PROFILE_IMG_MIME_TYPES = [
   'image/png',
   'image/webp',
 ];
+const SUPPORTED_SERVER_IMG_FORMATS_HUMAN = SUPPORTED_PROFILE_IMG_FORMATS_HUMAN;
+const SUPPORTED_SERVER_IMG_MIME_TYPES = SUPPORTED_PROFILE_IMG_MIME_TYPES;
 
 export {
   serverInviteRegex,
   exampleServerInviteLink,
+  SUPPORTED_PROFILE_IMG_FORMATS_HUMAN,
   SUPPORTED_PROFILE_IMG_MIME_TYPES,
+  SUPPORTED_SERVER_IMG_FORMATS_HUMAN,
+  SUPPORTED_SERVER_IMG_MIME_TYPES,
 };
