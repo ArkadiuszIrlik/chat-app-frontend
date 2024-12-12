de2c6dcadcd859a4778cbd1d0d823491b3642b8e
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2024-12-12 21:09:17 +0100

    Add SUPPORTED_PROFILE_IMG_MIME_TYPES constant

--- src/constants/apiData.ts
+++ src/constants/apiData.ts
@@ -2,5 +2,16 @@ import { getClientURL } from '@helpers/fetch';
 
 const serverInviteRegex = /(?:invite\/)([a-zA-Z0-9]{10})$|^([a-zA-Z0-9]{10})$/;
 const exampleServerInviteLink = getClientURL('app/invite/M0DQ26NLE7');
+const SUPPORTED_PROFILE_IMG_MIME_TYPES = [
+  'image/jpeg',
+  'image/avif',
+  'image/svg+xml',
+  'image/png',
+  'image/webp',
+];
 
-export { serverInviteRegex, exampleServerInviteLink };
+export {
+  serverInviteRegex,
+  exampleServerInviteLink,
+  SUPPORTED_PROFILE_IMG_MIME_TYPES,
+};
