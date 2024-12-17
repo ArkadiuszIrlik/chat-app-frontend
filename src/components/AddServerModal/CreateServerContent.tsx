b043620ddc725752db3c209a1117d2bb6a7a8b90
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2024-12-17 20:21:45 +0100

    Refactor AddServerModal component
    Refactored CreateServerContent inside AddServerModal. Server image
    field now allows both uploading an image, as well as selecting one
    from a list of presets. Fetch request has been updated with the new
    data structure expected by the API. Server name input field limits
    the max characters entered up to a limit allowed by the API.

--- src/components/AddServerModal/CreateServerContent.tsx
+++ src/components/AddServerModal/CreateServerContent.tsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from 'react';
 import { Form, Formik } from 'formik';
 import {
   ErrorDisplay,
-  ImageFileInput,
   SubmitButtonPrimary,
   SubmittingUpdater,
   TextInput,
@@ -12,6 +11,17 @@ import useFetch from '@hooks/useFetch';
 import { serverSchema } from '@constants/validationSchema';
 import { useServerStore } from '@hooks/index';
 import { useNavigate } from 'react-router-dom';
+import { ImageInput } from '@components/ImageInput';
+import { SUPPORTED_SERVER_IMG_MIME_TYPES } from '@constants/apiData';
+import usePresetPictures from '@components/AddServerModal/usePresetPictures';
+
+const initialValues = {
+  serverName: '',
+  selectServerImg: null,
+  uploadServerImg: null,
+  serverImg: '',
+  isUploadingServerImg: false,
+};
 
 function CreateServerContent({
   onNavigateBack,
@@ -22,14 +32,14 @@ function CreateServerContent({
   onCloseModal: () => void;
   onCloseServersMenu: () => void;
 }) {
-  const [postData, setPostData] = useState(new FormData());
+  const [postData, setPostData] = useState({});
+  const presetPictures = usePresetPictures();
 
   const { refetch, isLoading, data, hasError, errorMessage } = useFetch({
     initialUrl: 'servers',
     method: 'POST',
     onMount: false,
     postData,
-    isFileUpload: true,
   });
 
   const { addToStore } = useServerStore() ?? {};
@@ -50,39 +60,72 @@ function CreateServerContent({
 
   return (
     <Formik
-      initialValues={{
-        image: '',
-        name: '',
-      }}
+      initialValues={initialValues}
       validationSchema={Yup.object({
-        image: serverSchema.image.required(
-          'Please provide an image for your server.',
+        serverName: serverSchema.name.required(
+          'Please enter a name for your server',
         ),
-        name: serverSchema.name.required(
-          'Please enter a name for your server.',
+        serverImg: Yup.mixed().required(
+          'Please select or upload a server image',
         ),
+        selectServerImg: Yup.mixed().nullable(),
+        uploadServerImg: Yup.mixed()
+          .oneOfSchemas(
+            [
+              // this schema is here just to allow null values
+              Yup.string().nullable(),
+              serverSchema.image,
+            ],
+            { passNestedError: 1 },
+          )
+          .nullable(),
+        isUploadingServerImg: Yup.boolean()
+          .isFalse('Please wait while your server image is uploaded')
+          .required(),
       })}
       onSubmit={(values) => {
-        const formData = new FormData();
-        Object.entries(values).forEach(([key, value]) =>
-          formData.append(key, value),
-        );
-        setPostData(formData);
+        setPostData({
+          serverName: values.serverName,
+          serverImg: values.serverImg,
+        });
         refetch();
       }}
     >
       <Form>
         {hasError && <ErrorDisplay errorMessage={errorMessage} />}
         <div className="mb-1">
-          <ImageFileInput
-            textLabel="Server image"
-            buttonLabel="Upload server image"
-            name="image"
-            id="image"
+          <ImageInput
+            uploadedImageProps={{
+              name: 'uploadServerImg',
+              id: 'uploadServerImg',
+            }}
+            presetImageProps={{
+              name: 'selectServerImg',
+              id: 'selectServerImg',
+            }}
+            currentImageProps={{
+              name: 'serverImg',
+              id: 'serverImg',
+            }}
+            isUploadingImageProps={{
+              name: 'isUploadingServerImg',
+              id: 'isUploadingServerImg',
+            }}
+            imageUploadUrl="images/server-img"
+            presetImages={presetPictures}
+            sectionAriaLabel="Choose your server image"
+            sectionTextLabel="Server Image"
+            selectPresetModalLabel="Select Server Image"
+            acceptedFileFormats={SUPPORTED_SERVER_IMG_MIME_TYPES}
           />
         </div>
         <div className="mb-3">
-          <TextInput label="Server name" name="name" id="name" type="text" />
+          <TextInput
+            label="Server name"
+            name="serverName"
+            type="text"
+            maxLength={30}
+          />
         </div>
         <SubmittingUpdater isFetchLoading={isLoading} />
         <div className="mb-3 flex items-center gap-10">
