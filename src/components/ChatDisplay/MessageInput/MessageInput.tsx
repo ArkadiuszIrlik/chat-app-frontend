4a627a2950b0c012e4e041874d9b3364f921c856
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2025-01-10 18:48:49 +0100

    Clean up ChatDisplay directory

--- src/components/ChatDisplay/MessageInput/MessageInput.tsx
+++ src/components/ChatDisplay/MessageInput/MessageInput.tsx
@@ -4,9 +4,9 @@ import { useCallback, useEffect, useMemo, useState } from 'react';
 import Placeholder from '@tiptap/extension-placeholder';
 import { useParams } from 'react-router-dom';
 import { useAuth } from '@hooks/index';
-import InputRestoreManager from '@components/ChatDisplay/InputRestoreManager';
+import InputRestoreManager from '@components/ChatDisplay/MessageInput/InputRestoreManager';
 import useSendMessage from '@hooks/useSendMessage';
-import NewlineExtension from '@components/ChatDisplay/NewlineExtension';
+import NewlineExtension from '@components/ChatDisplay/MessageInput/NewlineExtension';
 
 const urlRegex =
   // eslint-disable-next-line no-useless-escape
