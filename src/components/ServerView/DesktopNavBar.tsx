0b25fcddc7e80f481cb5123ac94c8849c8aa998a
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2025-01-16 21:43:33 +0100

    Remove "Chats" option from DesktopNavBar
    Since the "Chats" options hasn't been implemented, the placeholder
    navigation link has been removed from the nav bar. Adjusted styling
    to make up for the missing part of the component.

--- src/components/ServerView/DesktopNavBar.tsx
+++ src/components/ServerView/DesktopNavBar.tsx
@@ -1,16 +1,10 @@
-import { Link } from 'react-router-dom';
 import ServersIcon from '@assets/servers-icon.png';
-import ChatsIcon from '@assets/chats-icon.png';
 import { ExtendedCSSProperties } from '@src/types';
 
 const serversStyles: ExtendedCSSProperties = {
   '--mask-url': `url(${ServersIcon})`,
 };
-const chatsStyles: ExtendedCSSProperties = {
-  '--mask-url': `url(${ChatsIcon})`,
-};
 
-const isActive = false;
 
 function DesktopNavBar({
   isServersMenuOpen,
@@ -20,7 +14,7 @@ function DesktopNavBar({
   onOpenServers: () => void;
 }) {
   return (
-    <div className="border-t-2 border-t-gray-500 pt-1">
+    <div className="mb-1 border-t-2 border-t-gray-500 pt-1">
       <div>
         <button
           type="button"
@@ -45,22 +39,6 @@ function DesktopNavBar({
             Servers
           </h5>
         </button>
-        <Link
-          to="/app/chats"
-          className={`group mb-1 flex w-full items-center gap-2 truncate rounded-md px-2 py-1 ${
-            isActive ? 'bg-gray-600 hover:bg-gray-500' : 'hover:bg-gray-600'
-          }`}
-        >
-          <div
-            className={`alpha-mask ${
-              isActive ? ' group-hover:bg-gray-300' : 'bg-gray-400'
-            } h-6 w-6 bg-gray-400 `}
-            style={chatsStyles}
-          />
-          <h5 className={`${isActive ? 'text-white' : 'text-gray-300'}`}>
-            Chats
-          </h5>
-        </Link>
       </div>
     </div>
   );
