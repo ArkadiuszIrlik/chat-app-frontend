68950b6e09e169084d39a92a222c709bb60583d5
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2025-03-12 18:17:33 +0100

    Add demo start button to home screen

--- src/containers/HomeScreen/ButtonPanel.tsx
+++ src/containers/HomeScreen/ButtonPanel.tsx
@@ -1,4 +1,5 @@
 import { PrimaryLink, SecondaryLink } from '@components/Link';
+import { Link } from 'react-router-dom';
 
 function ButtonPanel() {
   return (
@@ -6,6 +7,16 @@ function ButtonPanel() {
       className="mx-auto flex w-44 flex-col
       gap-4 sm:mr-0 sm:w-60 md:w-80"
     >
+      <div className="w-full shadow">
+        <Link
+          to="/demo"
+          className="block w-full rounded-md bg-gradient-to-tr from-clairvoyant-900 to-blue-700 px-5
+          py-2 text-center text-white shadow-[inset_0_2px_0_rgba(72,188,255,40%)] hover:brightness-110
+           active:brightness-125"
+        >
+          Take a tour
+        </Link>
+      </div>
       <PrimaryLink to="/login">Log in</PrimaryLink>
       <SecondaryLink to="/signup">Create account</SecondaryLink>
     </div>
